c5f2c1f7a2519a91631bcd459d60632561d2f61b
sj <64534668+sj-h4@users.noreply.github.com>
2026-01-30 22:27:37 +0900

    chore: move example code and add usage

--- README.md
+++ README.md
@@ -1,5 +1,36 @@
 # StructLens
 
+## Installation
+
+```bash
+# uv
+uv add git+https://github.com/naist-nlp/structlens.git
+# pip
+# pip install git+https://github.com/naist-nlp/structlens.git
+```
+
+## Usage
+
+```python
+import torch
+from structlens import L2DistanceSimilarityFunction, StructLens, create_masks
+
+num_tokens_per_layer = 5
+num_layers = 3
+hidden_size = 128
+
+representations = torch.randn(num_layers, num_tokens_per_layer, hidden_size)
+
+struct_lens = StructLens()
+st_list = struct_lens(representations)
+for i, st in enumerate(st_list):
+    print("layer: ", i)
+    print("max_score: ", st["max_score"])
+    print("argmax_heads: ", st["argmax_heads"])
+```
+
+See [examples/structlens-sample](examples/structlens-sample) for more details.
+
 ## Development
 
 ### Setup
--- examples/structlens-sample/main.py
+++ examples/structlens-sample/main.py
@@ -1,7 +1,7 @@
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-from structlens import L2DistanceSimilarityFunction, StructLens, create_masks
+from structlens import StructLens
 
 
 def main():
@@ -25,8 +25,6 @@ def main():
         outputs.hidden_states
     )  # Tuple of length 1 + num_layers (input embedding + num_layers)
     # residuals[i]: Tensor of shape (batch_size, num_tokens, hidden_size)
-    num_layers = len(residuals)
-    num_tokens_per_layer = residuals[0].shape[1]
 
     residuals_tensor = torch.stack(
         residuals
@@ -36,22 +34,8 @@ def main():
         1
     )  # (num_layers, num_tokens, hidden_size)
 
-    # mask for the scores
-    mask = create_masks(
-        num_nodes_per_layer=[num_tokens_per_layer] * num_layers,
-        device=representations.device,
-    )
-
     struct_lens = StructLens()
-    scores = struct_lens.compute_scores(
-        representations=representations,
-        mask=mask,
-        root_selection_scores=torch.zeros(num_layers, num_tokens_per_layer),
-        similarity_function=L2DistanceSimilarityFunction(),
-    )
-    st_list = struct_lens.compute_mst(
-        scores, num_nodes=[num_tokens_per_layer] * num_layers
-    )
+    st_list = struct_lens(representations)
     for i, st in enumerate(st_list):
         print("layer: ", i)
         print("argmax_heads: ", st["argmax_heads"])
